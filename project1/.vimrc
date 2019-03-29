cf1e228889a3c179abeaa2c81b857eb9b3fa3d9d
Aaron Ma <aaron.ma@panasonic.aero>
2019-03-29 15:59:56 -0700

    Worked on project1

--- project1/.vimrc
+++ project1/.vimrc
@@ -2,3 +2,6 @@ set background=dark
 set background=dark
 set background=dark
 set background=dark
+set background=dark
+set background=dark
+set background=dark
--- project1/application.py
+++ project1/application.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+API_KEY = "Bu3bld9w16f5oAK0XsaA"
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -25,7 +26,8 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     # Set website display variables
     if session.get("username") is None:
-        return render_template("login.html")
+        username = None
+        return render_template("login.html", username=username)
     return redirect(url_for('home'))
 
 @app.route("/login", methods=["GET", "POST"])
@@ -34,21 +36,27 @@ def login():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("signup.html")
+    username = None
+    return render_template("signup.html", username=username)
 
 @app.route("/createuser", methods=["POST"])
 def createuser():
+    if session.get("username") is None:
+        username = None
     request_username = request.form.get("request_username")
     request_password = request.form.get("request_password")
     repeat_password = request.form.get("repeat_password")
     if request_password != repeat_password:
-        return render_template("error.html", message="Passwords do not match, please try again.")
+        return render_template("error.html", message="Passwords do not match, please try again.", username=username, signuppage=True)
+    if request_username is "" or request_password is "":
+        return render_template("error.html", message="Not all fields are filled, please try again.", username=username, signuppage=True)
     if db.execute("SELECT * FROM users WHERE name = :name", {"name": request_username}).rowcount != 0:
-        return render_template("error.html", message="Username already exists, please try again.")
+        username = None
+        return render_template("error.html", message="Username already exists, please try again.", username=username, signuppage=True)
     db.execute("INSERT INTO users (name, password) VALUES (:name, :password)", {"name": request_username, "password": request_password})
     db.commit()
     session["username"] = request_username
-    return render_template("success.html", message="You have successfully signed up.")
+    return render_template("success.html", message="You have successfully signed up.", username=request_username, newuser=True)
 
 @app.route("/attemptlogin", methods=["POST"])
 def attemptlogin():
@@ -56,16 +64,18 @@ def attemptlogin():
     password = request.form.get("password")
     # Make sure username exists in database
     if db.execute("SELECT * FROM users WHERE name = :name", {"name": username}).rowcount == 0:
-        return render_template("error.html", message="Invalid user name, please sign up first.")
+        username = None
+        return render_template("error.html", message="Invalid user name, please sign up first.", username=username)
     # Make sure the password is correct
     passwords = db.execute("SELECT password FROM users WHERE name = :name", {"name": username})
     for row in passwords:
         if row[0] == password:
             session["username"] = username
             return redirect(url_for('home'))
-    return render_template("error.html", message="Your password is incorrect, please try again.")
+    username = None
+    return render_template("error.html", message="Your password is incorrect, please try again.", username=username)
 
-@app.route('/attemptlogout')
+@app.route("/attemptlogout")
 def logout():
    # remove the username from the session if it is there
    session.pop('username', None)
@@ -76,13 +86,46 @@ def home():
     username=session["username"]
     return render_template("home.html", username=username)
 
-@app.route("/search", methods=["POST"])
+@app.route("/search", methods=["GET", "POST"])
 def search():
+    username=session["username"]
     searchrequest = request.form.get("bookinfo")
     results = db.execute("SELECT * FROM books WHERE isbn LIKE '%' || :isbn || '%' OR title LIKE '%' || :title || '%' OR author LIKE '%' || :author || '%'", {"isbn": searchrequest, "title": searchrequest, "author": searchrequest}).fetchall()
-    return render_template("search.html", results=results)
+    numresults = len(results)
+    if numresults == 0:
+        return render_template("search.html", results=results, numresults=numresults, username=username)
+    return render_template("search.html", results=results, numresults=numresults, username=username)
 
-@app.route("/book")
-def book():
+@app.route("/book/")
+def allbooks():
+    username=session["username"]
+    results = db.execute("SELECT * FROM books").fetchall()
+    numresults = len(results)
+    return render_template("allbooks.html", results=results, numresults=numresults, username=username)
+
+@app.route("/book/<string:isbn>", methods=["GET"])
+def book(isbn):
+    username=session["username"]
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
+    if book is None:
+        return render_template("error.html", message="Book does not exist in our database.")
+    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": API_KEY, "isbns": isbn})
+    resJson = res.json()
+    try:
+        goodreadsrating = resJson['books'][0]['average_rating']
+        goodreadsnumreview = resJson['books'][0]['reviews_count']
+    except:
+        goodreadsrating = 'Does not exist on GoodReads'
+        goodreadsnumreview = 'Does not exist on GoodReads'
     # Set website display variables
-    return render_template("book.html")
+    return render_template("book.html", bookisbn=book[1], booktitle=book[2], bookauthor=book[3], bookyear=book[4], goodreadsrating=goodreadsrating, goodreadsnumreview=goodreadsnumreview, username=username)
+
+@app.route("/attemptreview", methods=["POST"])
+def attemptreview():
+    print('hehe2')
+    userrating = request.form.get("userrating")
+    userreview = request.form.get("userreview")
+    print(userrating)
+    print('hehe3')
+    print(userreview)
+    return render_template("success.html", message="Success! Your review has been received and posted to your ReviewBook.")
